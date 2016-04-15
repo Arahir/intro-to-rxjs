7d1fc08ec5bcbc84c89c5d165a12d9092fbebb02
matthieu faugere <matthieu.faugere@gmail.com>
2016-04-15 13:35:38 +0200

    CLean clone and fix refresh issue

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var requestStream = Rx.Observable.just('https://api.github.com/users');
+var refreshButton = document.querySelector('.refresh');
+
+var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+
+var requestStream = refreshClickStream
+  .startWith('startup click')
+  .map(function() {
+    var randomOffset = Math.floor(Math.random()*500);
+    return 'https://api.github.com/users?since=' + randomOffset;
+  });
 
 var responseStream = requestStream
   .flatMap(function(requestUrl) {
     return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl));
-  });
-
-var refreshButton = document.querySelector('.refresh');
-var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
-
-var requestStream = refreshClickStream
-  .map(function() {
-    var randomOffset = Math.floor(Math.random()*500);
-    return 'https://api.github.com/users?since=' + randomOffset;
   });
 
 var suggestion1Stream = responseStream

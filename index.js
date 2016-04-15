c70d7ccc599b43e8cdcef39d39735c85a8f05c34
matthieu faugere <matthieu.faugere@gmail.com>
2016-04-15 12:07:20 +0200

    Fix indentation

--- index.js
+++ index.js
@@ -18,30 +18,30 @@ var requestStream = refreshClickStream
     return 'https://api.github.com/users?since=' + randomOffset;
   });
 
-  var suggestion1Stream = responseStream
-    .map(function(listUsers) {
-      // get one random user from the list
-      return listUsers[Math.floor(Math.random()*listUsers.length)];
-    })
-    .merge(
-      refreshClickStream.map(function(){ return null; })
-    )
-    .startWith(null);
+var suggestion1Stream = responseStream
+  .map(function(listUsers) {
+    // get one random user from the list
+    return listUsers[Math.floor(Math.random()*listUsers.length)];
+  })
+  .merge(
+    refreshClickStream.map(function(){ return null; })
+  )
+  .startWith(null);
 
-  function renderSuggestion(suggestedUser, selector) {
-      var suggestionEl = document.querySelector(selector);
-      if (suggestedUser === null) {
-          suggestionEl.style.visibility = 'hidden';
-      } else {
-          suggestionEl.style.visibility = 'visible';
-          var usernameEl = suggestionEl.querySelector('.username');
-          usernameEl.href = suggestedUser.html_url;
-          usernameEl.textContent = suggestedUser.login;
-          var imgEl = suggestionEl.querySelector('img');
-          imgEl.src = "";
-          imgEl.src = suggestedUser.avatar_url;
-      }
-  }
-  suggestion1Stream.subscribe(function(suggestion) {
-    renderSuggestion(suggestion, '.suggestion1');
-  });
+function renderSuggestion(suggestedUser, selector) {
+    var suggestionEl = document.querySelector(selector);
+    if (suggestedUser === null) {
+        suggestionEl.style.visibility = 'hidden';
+    } else {
+        suggestionEl.style.visibility = 'visible';
+        var usernameEl = suggestionEl.querySelector('.username');
+        usernameEl.href = suggestedUser.html_url;
+        usernameEl.textContent = suggestedUser.login;
+        var imgEl = suggestionEl.querySelector('img');
+        imgEl.src = "";
+        imgEl.src = suggestedUser.avatar_url;
+    }
+}
+suggestion1Stream.subscribe(function(suggestion) {
+  renderSuggestion(suggestion, '.suggestion1');
+});
